449ab9a419cf7cbe83d4c551704a48dc8b38dd12
Arifcngk <ahey5241@gmail.com>
2024-12-26 23:17:27 +0300

    feat : Auth UI added

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:eticaret_fullstack/views/screens/aurh_screens/login..dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Ticaret Fullstack',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
